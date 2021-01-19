fb9433d0ace43b656b8174afc5c00bedac05ca87
Marko Hietamäki <Marko.Hietamaki@seamk.fi>
2021-01-19 15:14:55 +0200

    Add project files.

---
+++ Olion_serialisointi_AUTE19SA/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Olion_serialisointi_AUTE19SA
+{
+    class Program
+    {
+        private static Viesti v;
+
+        static void Main(string[] args)
+        {
+            v = new Viesti();
+            v.lahettaja = "Matti";
+            v.vastaanottaja = "Teppo";
+            v.viesti = "Mukavaa hiihtoloman odotusta";
+
+            Tallenna();
+            Avaa();
+            Console.ReadKey();
+        }
+        public static void Tallenna()
+        {
+            string json = JsonConvert.SerializeObject(v,Formatting.Indented);
+            //Console.WriteLine(json);
+            File.WriteAllText(@"c:\viesti.json", json);
+        }
+        public static void Avaa()
+        {
+            Viesti v2 = JsonConvert.DeserializeObject<Viesti>(File.ReadAllText(@"c:\viesti.json"));
+            Console.WriteLine("v2 " + v.lahettaja + " " + v.vastaanottaja + " " + v.viesti + " " + v.kuitattu);
+        }
+    }
+}
---
+++ Olion_serialisointi_AUTE19SA/Viesti.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Olion_serialisointi_AUTE19SA
+{
+    public class Viesti
+    {
+      
+        public int kuitattu { get; set; } = 0; //0=default
+        public string lahettaja { get; set; }
+        public string vastaanottaja { get; set; }
+        public string viesti { get; set; }
+    }
+}
